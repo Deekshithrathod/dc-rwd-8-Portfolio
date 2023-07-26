4a3bee31dbf70c89e06c0b741c2cdb34732a4896
deekshith <deekshith@Mac-mini.local>
2023-07-26 18:48:29 +0530

    Working first-screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
+import { Link } from "react-router-dom";
 import "./App.css";
-import AllComponents from "./pages/AllComponents";
 
 function App() {
   return (
     <>
-      <AllComponents />
+      <div className="home">
+        <div className="button-container">
+          <Link to={"/v1"}>
+            <button className="btn">Layout 1</button>
+          </Link>
+          <Link to={"/v2"}>
+            <button className="button-special btn">Layout 2</button>
+          </Link>
+        </div>
+      </div>
+      {/* <AllComponents /> */}
     </>
   );
 }
