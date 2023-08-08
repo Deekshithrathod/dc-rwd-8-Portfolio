3df214ff07695e9d71f24e99c49da3aca1ac961e
deekshith <deekshith@Mac-mini.local>
2023-08-08 21:34:15 +0530

    made the ProjectCard component functional

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -1,36 +1,41 @@
-import { ProfileCardProps } from "../ProfileCard/ProfileCard";
 import "./ProjectCard.css";
+import { orientationProp } from "../../common/commonType";
 
-const projectDetails = {
-  image: `project.png`,
-  techStack: `#HTML #CSS #responsive`,
-  title: `Landing Page`,
-  desc: `In this project, I work with HTML and CSS to create a responsive page
-  . The design is from devchallenge.io. Donec aliquam est dui, vel
-  vestibulum diam sollicitudin id. Quisque feugiat malesuada molestie.`,
-  demoLink: `https://devchallenges-rwd-7.onrender.com`,
-  codeLink: `https://github.com/Deekshithrathod/dc-rwd-7-landing-page`,
-};
+export interface Project {
+  title: string;
+  desc: string;
+  techStack: string;
+  image: string;
+  demoLink: string;
+  codeLink: string;
+}
+type ProjectProps = orientationProp & Project;
 
-const ProjectCard = (props: ProfileCardProps) => {
+const ProjectCard = ({
+  orientation,
+  title,
+  desc,
+  image,
+  techStack,
+  demoLink,
+  codeLink,
+}: ProjectProps) => {
   return (
-    <div className={`project-card-${props.orientation}`}>
+    <div className={`project-card-${orientation}`}>
       <div className="project-image">
-        <img src={projectDetails.image} />
+        <img src={image} />
       </div>
       <div className="project-details">
-        <div className="tech-stack">{projectDetails.techStack}</div>
-        <div className="project-title">{projectDetails.title}</div>
+        <div className="tech-stack">{techStack}</div>
+        <div className="project-title">{title}</div>
         <div className="project-desc">
-          {props.orientation === "vertical"
-            ? projectDetails.desc.slice(0, 108)
-            : projectDetails.desc}
+          {orientation === "vertical" ? desc.slice(0, 108) : desc}
         </div>
         <div className="project-buttons">
-          <a href={projectDetails.demoLink} target="_blank">
+          <a href={demoLink} target="_blank">
             <button>Demo</button>
           </a>
-          <a href={projectDetails.codeLink} target="_blank">
+          <a href={codeLink} target="_blank">
             <button>Code</button>
           </a>
         </div>
