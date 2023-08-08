fb75b4562c9e0cc6599b17cff17d2897e4ec65ce
deekshith <deekshith@Mac-mini.local>
2023-08-08 18:36:38 +0530

    removed dead code & updated Experiences card

--- src/components/ExperiencesCard/ExperiencesCard.tsx
+++ src/components/ExperiencesCard/ExperiencesCard.tsx
@@ -1,30 +1,30 @@
-import { ProfileCardProps } from "../ProfileCard/ProfileCard";
 import "./ExperiencesCard.css";
+import { orientationProp } from "../../common/commonType";
 
 type experience = { time: string; role: string; desc: string; logo: string };
 
 const experiencesList: experience[] = [
   {
-    time: "Feb 2017 - Current",
-    role: "Front-end developer",
-    desc: "Donec aliquam est dui, vel vestibulum diam sollicitudin id. Quisque feugiat malesuada molestie.",
+    time: "Jan 2022 - Oct 2022",
+    role: "Member of Technical Staff",
+    desc: "Created a streamlined license management RESTful API with OpenAPI, OpenAPI-maven-plugin, and Swagger, optimizing efficiency and reliability while developing a performance-enhanced stress testing tool using Java, Spring Boot, and Kotlin with multi-threading.",
     logo: "comp-2.png",
   },
   {
-    time: "Aug 2016 - Feb 2018",
-    role: "Full-stack developer",
-    desc: "Donec aliquam est dui, vel vestibulum diam sollicitudin id. Quisque feugiat malesuada molestie.",
+    time: "Sept 2021 - Jan 2022",
+    role: "Member of Technical Staff (Intern)",
+    desc: "Streamlined employee access to company resources by implementing OAuth2.0 and Passport.js for a seamless single sign-on solution, removing the requirement for separate login credentials.",
     logo: "comp-2.png",
   },
   {
-    time: "Jun 2015 - Aug 2016",
-    role: "Junior front-end",
-    desc: "Donec aliquam est dui, vel vestibulum diam sollicitudin id. Quisque feugiat malesuada molestie.",
+    time: "Feb 2020 - Feb 2021",
+    role: "Marketing Automation Engineer",
+    desc: `Contributed to multi-channel digital marketing campaigns, employing Beautiful Soup for web data collection, competitor analysis, and growth insights, while utilizing Google dashboards to track and analyze campaign performance through data from Google Analytics.`,
     logo: "comp-1.png",
   },
 ];
 
-const ExperiencesCard = (props: ProfileCardProps) => {
+const ExperiencesCard = (props: orientationProp) => {
   return (
     <div className={`exp-card-${props.orientation}`}>
       <h1>Experiences</h1>
